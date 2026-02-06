8fb101aadcd1444868ff12e3999c259b9201fc4f
Tim Kersey <t@k-t.im>
2026-02-05 21:51:33 -0800

    Use git revision source strategy for bz formula

--- Formula/bz.rb
+++ Formula/bz.rb
@@ -1,8 +1,10 @@
 class Bz < Formula
   desc "Zig implementation of the br protocol"
-  homepage "https://github.com/tkersey/bz"
-  url "https://github.com/tkersey/bz/archive/2e12026fb4c041142c173887d353e35067c4ef33.tar.gz"
-  sha256 "23b8f7fa4cf74d17c0e181335fa1f2267477a844306f220351ac5992e8ad8d4d"
+  homepage "https://github.com/tkersey/homebrew-bz"
+  url "https://github.com/tkersey/bz.git",
+      using:    :git,
+      revision: "2e12026fb4c041142c173887d353e35067c4ef33"
+  version "2e12026"
   license "MIT"
   head "https://github.com/tkersey/bz.git", branch: "main"
 
